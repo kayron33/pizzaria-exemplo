4e1927f96fa689a9f74d0a42bddb40e3621c843c
kayron33 <129231712+kayron33@users.noreply.github.com>
2024-04-03 00:39:00 -0300

    atualização

--- site/asset/js/ms.js
+++ site/asset/js/ms.js
@@ -14,4 +14,17 @@ document.querySelector("#login").onclick = () =>{
     carrinho.classList.remove('active');
 }
 
-var swiper =new
+var swiper = new Swiper(".home-slider", {
+    autoplay:{
+        delay:2500,
+        disableOnInteraction: false,
+    },
+    grapCursor:true,
+    loop:true,
+    centeredSlides:true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+})
+
