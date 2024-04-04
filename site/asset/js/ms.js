1a54ba99cb907af618339c714ec8dcfc1ea6b49c
kayron33 <129231712+kayron33@users.noreply.github.com>
2024-04-04 12:13:44 -0300

    correçao

--- site/asset/js/ms.js
+++ site/asset/js/ms.js
@@ -14,18 +14,33 @@ document.querySelector("#login").onclick = () =>{
     carrinho.classList.remove('active');
 }
 
+var swiper = new Swiper(".home-slider",{
+    autoplay:{
+        delay:2500,
+        disableOnInteraction: false,
+    },
+    grapCursor:true,
+    loop:true,
+    centeredSlides:true,
+    navigation:{
+        nextEl:'.swiper-button-next',
+        prevEl:'.swiper-button-prev',
+    }
+
+})
 
 
-//ABA DE MENU//
-var swiper = new Swiper(".menu-slider", {
+
+var swiper = new Swiper(".menu-slider",{
     grapCursor:true,
     loop:true,
     autoHeight:true,
     centeredSlides:true,
-    spaceBetwwen:20,
+    spaceBetwwem:20,
     pagination: {
         el: '.swiper-pagination',
         clickable:true,
       },
+
 })
 
